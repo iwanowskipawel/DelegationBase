59b983586a75760dcf5f4323554a33dd6a7d0d3f
Paweł Iwanowski <iwanowski3.14@gmail.com>
2021-01-29 12:15:19 +0100

    Add DbContext to startup service configuration

--- Delegation/Startup.cs
+++ Delegation/Startup.cs
@@ -10,16 +10,26 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Delegation.Models.Repositories;
 using Delegation.Models.FakeRepositories;
+using Microsoft.Extensions.Configuration;
+using Delegation.Models.DataAccess;
+using Microsoft.EntityFrameworkCore;
 
 namespace Delegation
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration) => Configuration = configuration;
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<IBusinessTripRepository, FakeBusinessTripRepository>();
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+                Configuration["Data:DelegationBaseBusinessTrips:ConnectionString"]
+                ));
+            services.AddTransient<IBusinessTripRepository, EFBusinessTripRepository>();
             services.AddMvc();
             services.AddControllers();
         }
