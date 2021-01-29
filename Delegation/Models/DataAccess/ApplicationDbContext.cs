c3bedf11525141bbb49ce381f7a22c05e4fc88c4
Paweł Iwanowski <iwanowski3.14@gmail.com>
2021-01-29 12:07:09 +0100

    Add Data Access and connection string to appsettings

---
+++ Delegation/Models/DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Delegation.Models.DataAccess
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<BusinessTrip> BusinessTrips { get; set; }
+    }
+}
---
+++ Delegation/Models/DataAccess/EFBusinessTripRepository.cs
@@ -0,0 +1,17 @@
+﻿using Delegation.Models.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Delegation.Models.DataAccess
+{
+    public class EFBusinessTripRepository : IBusinessTripRepository
+    {
+        private ApplicationDbContext _context;
+
+        public EFBusinessTripRepository(ApplicationDbContext context) => _context = context;
+
+        public IQueryable<BusinessTrip> BusinessTrips => _context.BusinessTrips;
+    }
+}
