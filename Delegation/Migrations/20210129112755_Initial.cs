7845840740470a40ff11c67a6d41e584bc665ae7
Paweł Iwanowski <iwanowski3.14@gmail.com>
2021-01-29 12:28:13 +0100

    Add migrations

---
+++ Delegation/Migrations/20210129112755_Initial.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Delegation.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Address",
+                columns: table => new
+                {
+                    AddressID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Street = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Number = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PostalCode = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Address", x => x.AddressID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    EmployeeID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Discriminator = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DriverID = table.Column<int>(type: "int", nullable: true),
+                    PersonID = table.Column<int>(type: "int", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.EmployeeID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Company",
+                columns: table => new
+                {
+                    CompanyID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AddressID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Company", x => x.CompanyID);
+                    table.ForeignKey(
+                        name: "FK_Company_Address_AddressID",
+                        column: x => x.AddressID,
+                        principalTable: "Address",
+                        principalColumn: "AddressID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Destination",
+                columns: table => new
+                {
+                    DestinationID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AddressID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Destination", x => x.DestinationID);
+                    table.ForeignKey(
+                        name: "FK_Destination_Address_AddressID",
+                        column: x => x.AddressID,
+                        principalTable: "Address",
+                        principalColumn: "AddressID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Car",
+                columns: table => new
+                {
+                    CarID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Model = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RegistrationNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MainDriverEmployeeID = table.Column<int>(type: "int", nullable: true),
+                    MeterStatus = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Car", x => x.CarID);
+                    table.ForeignKey(
+                        name: "FK_Car_Employee_MainDriverEmployeeID",
+                        column: x => x.MainDriverEmployeeID,
+                        principalTable: "Employee",
+                        principalColumn: "EmployeeID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Project",
+                columns: table => new
+                {
+                    ProjectID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Symbol = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CompanyID = table.Column<int>(type: "int", nullable: true),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Project", x => x.ProjectID);
+                    table.ForeignKey(
+                        name: "FK_Project_Company_CompanyID",
+                        column: x => x.CompanyID,
+                        principalTable: "Company",
+                        principalColumn: "CompanyID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "KilometersCard",
+                columns: table => new
+                {
+                    KilometersCardID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CardSymbol = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    WorkCardNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CarID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_KilometersCard", x => x.KilometersCardID);
+                    table.ForeignKey(
+                        name: "FK_KilometersCard_Car_CarID",
+                        column: x => x.CarID,
+                        principalTable: "Car",
+                        principalColumn: "CarID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BusinessTrips",
+                columns: table => new
+                {
+                    BusinessTripID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DepartureDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ArrivalDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DriverEmployeeID = table.Column<int>(type: "int", nullable: true),
+                    ProjectID = table.Column<int>(type: "int", nullable: true),
+                    KeeperEmployeeID = table.Column<int>(type: "int", nullable: true),
+                    DestinationID = table.Column<int>(type: "int", nullable: true),
+                    CarID = table.Column<int>(type: "int", nullable: true),
+                    InitialMeter = table.Column<int>(type: "int", nullable: false),
+                    FinalMeter = table.Column<int>(type: "int", nullable: false),
+                    KilometersCardID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BusinessTrips", x => x.BusinessTripID);
+                    table.ForeignKey(
+                        name: "FK_BusinessTrips_Car_CarID",
+                        column: x => x.CarID,
+                        principalTable: "Car",
+                        principalColumn: "CarID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_BusinessTrips_Destination_DestinationID",
+                        column: x => x.DestinationID,
+                        principalTable: "Destination",
+                        principalColumn: "DestinationID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_BusinessTrips_Employee_DriverEmployeeID",
+                        column: x => x.DriverEmployeeID,
+                        principalTable: "Employee",
+                        principalColumn: "EmployeeID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_BusinessTrips_Employee_KeeperEmployeeID",
+                        column: x => x.KeeperEmployeeID,
+                        principalTable: "Employee",
+                        principalColumn: "EmployeeID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_BusinessTrips_KilometersCard_KilometersCardID",
+                        column: x => x.KilometersCardID,
+                        principalTable: "KilometersCard",
+                        principalColumn: "KilometersCardID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_BusinessTrips_Project_ProjectID",
+                        column: x => x.ProjectID,
+                        principalTable: "Project",
+                        principalColumn: "ProjectID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BusinessTrips_CarID",
+                table: "BusinessTrips",
+                column: "CarID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BusinessTrips_DestinationID",
+                table: "BusinessTrips",
+                column: "DestinationID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BusinessTrips_DriverEmployeeID",
+                table: "BusinessTrips",
+                column: "DriverEmployeeID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BusinessTrips_KeeperEmployeeID",
+                table: "BusinessTrips",
+                column: "KeeperEmployeeID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BusinessTrips_KilometersCardID",
+                table: "BusinessTrips",
+                column: "KilometersCardID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BusinessTrips_ProjectID",
+                table: "BusinessTrips",
+                column: "ProjectID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Car_MainDriverEmployeeID",
+                table: "Car",
+                column: "MainDriverEmployeeID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Company_AddressID",
+                table: "Company",
+                column: "AddressID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Destination_AddressID",
+                table: "Destination",
+                column: "AddressID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_KilometersCard_CarID",
+                table: "KilometersCard",
+                column: "CarID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Project_CompanyID",
+                table: "Project",
+                column: "CompanyID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BusinessTrips");
+
+            migrationBuilder.DropTable(
+                name: "Destination");
+
+            migrationBuilder.DropTable(
+                name: "KilometersCard");
+
+            migrationBuilder.DropTable(
+                name: "Project");
+
+            migrationBuilder.DropTable(
+                name: "Car");
+
+            migrationBuilder.DropTable(
+                name: "Company");
+
+            migrationBuilder.DropTable(
+                name: "Employee");
+
+            migrationBuilder.DropTable(
+                name: "Address");
+        }
+    }
+}
