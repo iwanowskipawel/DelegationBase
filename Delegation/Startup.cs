43a91b59603e8adfccb4495ad398a03a0d780c45
Paweł Iwanowski <iwanowski3.14@gmail.com>
2021-01-29 10:29:50 +0100

    Register repository in Startup.cs

--- Delegation/Startup.cs
+++ Delegation/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Delegation.Models.Repositories;
+using Delegation.Models.FakeRepositories;
 
 namespace Delegation
 {
@@ -17,6 +19,7 @@ namespace Delegation
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IBusinessTripRepository, FakeBusinessTripRepository>();
             services.AddMvc(options => options.EnableEndpointRouting = false);
             services.AddControllers(options => options.EnableEndpointRouting = false);
         }
