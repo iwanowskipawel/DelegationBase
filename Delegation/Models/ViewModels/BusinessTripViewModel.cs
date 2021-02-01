407f05e1953a8a88bbba244d44e72c5d567355b0
Paweł Iwanowski <iwanowski3.14@gmail.com>
2021-02-01 10:49:10 +0100

    Added BusinessTripViewModel

---
+++ Delegation/Models/ViewModels/BusinessTripViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Delegation.Models.ViewModels
+{
+    public class BusinessTripViewModel
+    {
+        public IEnumerable<BusinessTrip> BusinessTrips { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
