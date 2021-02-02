bf4ff56a325507362af72ee4a158df024d95903e
Paweł Iwanowski <iwanowski3.14@gmail.com>
2021-02-02 08:20:49 +0100

    Add Manage Business Trip Controller class

---
+++ Delegation/Controllers/ManageBusinessTripController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Delegation.Models.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Delegation.Controllers
+{
+    public class ManageBusinessTripController : Controller
+    {
+        IBusinessTripRepository _businessTripRepository;
+
+        ManageBusinessTripController(IBusinessTripRepository repository)
+            => _businessTripRepository = repository;
+
+        public ViewResult Index() => View(_businessTripRepository.BusinessTrips);
+    }
+}
