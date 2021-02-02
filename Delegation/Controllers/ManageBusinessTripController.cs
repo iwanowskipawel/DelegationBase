cd21ad4d654829bfd6e3d3802e7cc2889264f2e7
Paweł Iwanowski <iwanowski3.14@gmail.com>
2021-02-02 13:57:34 +0100

    Added ManageBusinessTripViewModel

--- Delegation/Controllers/ManageBusinessTripController.cs
+++ Delegation/Controllers/ManageBusinessTripController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Delegation.Models.Repositories;
+using Delegation.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Delegation.Controllers
@@ -10,14 +12,21 @@ namespace Delegation.Controllers
     public class ManageBusinessTripController : Controller
     {
         IBusinessTripRepository _repository;
-
+        
         public ManageBusinessTripController(IBusinessTripRepository repository)
             => _repository = repository;
 
         public ViewResult Index() => View(_repository.BusinessTrips);
 
         public ViewResult Edit(int businessTripID)
-            => View(_repository.BusinessTrips
-                .FirstOrDefault(t => t.BusinessTripID == businessTripID));
+            => View(new ManageBusinessTripViewModel
+            {
+                BusinessTrip = _repository.BusinessTrips
+                    .FirstOrDefault(t => t.BusinessTripID == businessTripID),
+                Drivers = _repository.BusinessTrips
+                    .Select(d => d.Driver)
+                    .Distinct()
+                    .OrderBy(d => d.EmployeeID)
+            });
     }
 }
---
+++ Delegation/Models/ViewModels/ManageBusinessTripViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Delegation.Models.ViewModels
+{
+    public class ManageBusinessTripViewModel
+    {
+        public BusinessTrip BusinessTrip { get; set; }
+        public string SelectedDriver { get; set; }
+        public IEnumerable<SelectListItem> Drivers { get; set; }
+        public string SelectedProject { get; set; }
+        public IEnumerable<SelectListItem> Projects { get; set; }
+    }
+}
